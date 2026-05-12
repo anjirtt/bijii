fe5f329de0f9d3d959e53c94b6841651f83a1ba1
anjirtt <sempakloer52@gmail.com>
2026-05-12 11:38:08 +0700

    Update environment variable defaults in config.py

--- fansx/config.py
+++ fansx/config.py
@@ -5,15 +5,15 @@ load_dotenv(".env")
 
 MAX_BOT = int(os.getenv("MAX_BOT", "400"))
 
-DEVS = list(map(int, os.getenv("DEVS", "7837317427").split()))
+DEVS = list(map(int, os.getenv("DEVS", "8711726001").split()))
 
-API_ID = int(os.getenv("API_ID", "35549269"))
+API_ID = int(os.getenv("API_ID", "31744107"))
 
-API_HASH = os.getenv("API_HASH", "c141efa27296c0c068172c34d847a758")
+API_HASH = os.getenv("API_HASH", "7e48044f6cca92da38d7e1bad87ad096")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8537047218:AAGDJQz8cKmVJDa7_MXjmTrZtZUGCQCXo9w")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8648903246:AAEGypVK4WxL4vtOT3fMY47gVgSdYN3fsGY")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "7837317427"))
+OWNER_ID = int(os.getenv("OWNER_ID", "8711726001"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1003192483568").split()))
 
@@ -21,4 +21,4 @@ RMBG_API = os.getenv("RMBG_API", "f8N4KnJwDK2nbJ6wZ2D6tweg")
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://danargntng34_db_user:LbTitJc30N2qJnRA@cluster0.zl4jybb.mongodb.net/?appName=Cluster0")
 
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1003411535752"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1003394338696"))
